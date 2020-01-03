15d06cb261bb8982a23caddbb0c316160a212935
user.email <samesense@gmail.com>
2020-01-03 14:25:52 -0500

    Add concat method.

--- src/ctr/__init__.py
+++ src/ctr/__init__.py
@@ -1,1 +1,8 @@
 __version__ = '0.0.0'
+from snakemake import shell
+
+def concat(file_list, output_file):
+     f = file_list[0]
+     shell('head -1 {f} > {output_file}')
+     for afile in file_list:
+         shell('tail -n +2 {afile} >> {output_file}')
