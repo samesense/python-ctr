d7b8c2e2aa60197063aea1a63f4c1950b52b3fee
user.email <samesense@gmail.com>
2020-01-06 16:48:15 -0500

    Bump version: 0.0.1 → 0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'ctr'
 year = '2019-2020'
 author = 'Perry Evans'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.1'
+version = release = '0.0.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
