9f45520a8ab5bc7a362e920163bcc0b93353f62e
user.email <samesense@gmail.com>
2020-01-06 21:39:39 -0500

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='ctr',
-    version='0.0.5',
+    version='0.0.6',
     license='MIT',
     description='Concatenate files w/ headers.',
     long_description='%s\n%s' % (
