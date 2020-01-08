83e1221672249f3ebb0f56e8c1ca0ebeacea4a50
user.email <samesense@gmail.com>
2020-01-08 15:29:16 -0500

    Testing.

---
+++ src/ctr/tests/test_ctr.py
@@ -0,0 +1,4 @@
+import ctr
+
+def test_concat():
+    return assert 1==1
--- tests/test_ctr.py
+++ tests/test_ctr.py
@@ -1,6 +1,25 @@
-
+import sys
 from ctr.cli import main
-
+from unittest.mock import patch
 
 def test_main():
-    main([])
+    with open('in1', 'w') as fout:
+        print('h1\th2', file=fout)
+        print('1\t2', file=fout)
+
+    with open('in2', 'w') as fout:
+        print('h1\th2', file=fout)
+        print('3\t4', file=fout)
+
+    testargs = ['ctr', 'outfile', 'in1', 'in2']
+    with patch.object(sys, 'argv', testargs):
+        main(sys.argv)
+
+        with open('outfile') as f:
+            line = f.readline()
+            assert line=='h1\th2'
+            line = f.readline()
+            assert line== '1\t2'
+            line = f.readline()
+            assert line== '3\t4'
+    os.system('rm in1 in2')
